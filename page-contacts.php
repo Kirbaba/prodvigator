27508f8c7d3ce7e91eaccb5349e6ed212080d723
Vladislav <v.kirbaba@gmail.com>
2016-05-17 14:04:01 +0300

    Контакты - старт

---
+++ page-contacts.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?> 
+
+	<!-- open .contacts-banner -->
+	<section class="contacts-banner">
+		<!-- open .container -->
+		<div class="container">
+			
+		</div>
+		<!-- close .container -->
+	</section>
+	<!-- close .contacts-banner -->
+
+<?php get_template_part('partners'); ?>
+<?php get_template_part('sitemap'); ?>
+<?php get_template_part('contact-info'); ?>
+<?php get_footer(); ?> 
