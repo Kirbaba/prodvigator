3e9a8075ae64410ff16ac9562a4a8ef0fe1b7666
Vladislav <v.kirbaba@gmail.com>
2016-05-30 12:44:25 +0300

    Футер

--- footer.php
+++ footer.php
@@ -14,22 +14,19 @@
     <!-- close .container -->
 </section>
 <!-- close .sitemap -->
-<!-- open .contact-info -->
-<section class="contact-info">
-    <!-- open .container -->
-    <div class="container">
-        <h3>Контакты:</h3>
-        <h3><?= fw_get_db_customizer_option( 'address' ); ?></h3>
-        <h3><?= fw_get_db_customizer_option( 'second_phone' ); ?></h3>
 
-        <a href="#" class="contact-info_link">Карта проезда</a>
-    </div>
-    <!-- close .container -->
-</section>
-<!-- close .contact-info -->
 <footer class="footer">
     <!-- open .container -->
     <div class="container">
+        <!-- open .contact-info -->
+        <article class="contact-info">
+                <h3>Контакты:</h3>
+                <h3><?= fw_get_db_customizer_option( 'address' ); ?></h3>
+                <h3><?= fw_get_db_customizer_option( 'second_phone' ); ?></h3>
+
+                <a href="#" class="contact-info_link">Карта проезда</a>    
+        </article>
+        <!-- close .contact-info -->
         <!-- open .footer__flex -->
         <div class="footer__flex">
             <!-- open .footer__contact -->
@@ -37,12 +34,13 @@
                 <h3>Электронная почта: <br/><a
                         href="mailto:<?= fw_get_db_customizer_option( 'email' ); ?>"><?= fw_get_db_customizer_option( 'email' ); ?></a>
                 </h3>
+                <a href="/" class="footer__logo">
+                    <img src="<?php $logo = fw_get_db_customizer_option( 'logo' );
+                    echo $logo['url']; ?>" alt=""/>
+                </a>
             </div>
             <!-- close .footer__contact -->
-            <a href="/" class="footer__logo">
-                <img src="<?php $logo = fw_get_db_customizer_option( 'logo' );
-                echo $logo['url']; ?>" alt=""/>
-            </a>
+            
         </div>
         <!-- close .footer__flex -->
     </div>
